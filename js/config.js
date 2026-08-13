e38ec5c9fa900fab9447bdae0f2f5533eb9948ea
evoluaoneerp <evoluaoneerp@gmail.com>
2026-08-13 16:49:17 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@ export const SUPABASE_URL = 'https://rgbsyrefpawftgohpxqg.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_-UDNdGypgbljylhBdVTtqw_p0SKQky3';
 export const APP_NAME = 'Evolua One';
 export const CRM_URL = 'https://evoluamaisprofissoes.github.io/evolua-crm-comercial/';
-export const VERSION = '0.2.2';
+export const VERSION = '0.3.0';
